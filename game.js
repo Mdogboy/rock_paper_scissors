d126c4e1e8b34b8072d1577bbb80db77686e2b3e
Michael <mandokim@live.com>
2022-06-08 02:50:37 +0800

    Complete game() function in game.js

--- game.js
+++ game.js
@@ -19,7 +19,7 @@ function playRound(playerChoice, computerChoice){
     if(playerChoiceLower == "rock" && computerChoiceLower == "scissors" ||
     playerChoiceLower == "scissors" && computerChoiceLower == "paper" ||
     playerChoiceLower == "paper" && computerChoiceLower == "rock"){
-        return `You win! ${playerChoice} beats ${computerChoice}`;
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
     }
     else if(computerChoiceLower == "rock" && playerChoiceLower == "scissors" ||
     computerChoiceLower == "scissors" && playerChoiceLower == "paper" ||
@@ -28,5 +28,36 @@ function playRound(playerChoice, computerChoice){
     }
     else{
         return "Tie game!";
+    }
+}
+
+function game(){
+    const MAX_ROUNDS = 5;
+    let playerWins = 0;
+    let computerWins = 0;
+
+    for(let i = 0; i < MAX_ROUNDS; i++){
+        let playerChoice = prompt("Rock, Paper, Scissors?");
+        let computerChoice = computerPlay();
+        let result = playRound(playerChoice, computerChoice);
+
+        console.log(result);
+
+        if(result.includes("Win")){
+            playerWins++;
+        }
+        else if(result.includes("Lose")){
+            computerWins++;
+        }
+    }
+
+    if(playerWins > computerWins){
+        console.log("You win the game!");
+    }
+    else if (playerWins < computerWins){
+        console.log("You lose the game!");
+    }
+    else{
+        console.log("Tie game!");
     }
 }
