09d48765d32070637334f899d3ded93312ad9097
Michael <mandokim@live.com>
2022-06-08 02:21:02 +0800

    Create computerPlay() function in game.js

---
+++ game.js
@@ -0,0 +1,19 @@
+function computerPlay(){
+    let random = Math.random() * 3;
+
+    if(random < 1){
+        return "Rock";
+    }
+    else if (random < 2){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
+
+function playRound(playerChoice, computerChoice){
+    if(playerChoice == "Rock"){
+        
+    }
+}
